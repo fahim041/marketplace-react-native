1bc45b20e9bd196a8f77b66a3b660941a1a2be53
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2023-07-02 11:13:58 -0600

    default text styles & AppText Component for text input

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import AccountScreen from './app/screens/AccountScreen';
-import ListingScreen from './app/screens/ListingScreen';
+import { Switch, TextInput } from 'react-native';
+import { useState } from 'react';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return <ListingScreen />;
+  const [isNew, setIsNew] = useState(false);
+  return (
+    <Screen>
+      <Switch value={isNew} onValueChange={(val) => setIsNew(val)} />
+    </Screen>
+  );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,12 +1,6 @@
-import { Text, StyleSheet, Platform } from 'react-native';
+import { Text } from 'react-native';
+import defaultStyles from '../config/styles';
 
 export default function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-  },
-});
